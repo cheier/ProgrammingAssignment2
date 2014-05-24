23ee40e80ecf6a0039e0fb2f761ca05f94b102cb
cheier <chris@chrisheier.com>
2014-05-23 23:19:01 -0600

    - Fixed typos. - Changed function names in makeCacheMatrix() to be more generic - Applied changes in function names to cacheSolve() - Added more information in the function definitions

--- cachematrix.R
+++ cachematrix.R
@@ -1,9 +1,11 @@
 ## Programming Assignment 2 - Lexical Scoping
 ##
 ## makeCacheMatrix(x = matrix())
-##      Generates a list of functions with data for a cache
-##      matrix inversion solver. Initial use will result in
-##      working data with a NULL result.
+##      Creates a list environment containing 4 functions for storing
+##      working data and caching computed data. This function is
+##      primarily used in conjunction with appropriate computational 
+##      functions in order to reduce computing time where repeated
+##      calculations will net the same results.
 ##
 ##      Example:
 ##      x <- makeCacheMatrix(matrix(runif(100, 1, 100), 10, 10))
@@ -12,12 +14,17 @@
 ##      10 x 10 matrix populated with random numbers between
 ##      1 and 100.
 ##      
-##      The following functions are created fpr the list environment:
+##      The following functions are created for the list environment:
 ##
 ##      - $set(y) -> Resets computational output and loads new data
 ##      - $get() -> Retreives working data
-##      - $setSolve(solve) -> Write computed data back to cache
-##      - $getSolve() -> Retreives cached computed data
+##      - $setCompData(compData) -> Write computed data back to cache
+##      - $getCompData() -> Retreives cached computed data
+##
+##      Initial running of this function will set computed data to a
+##      NULL value. It is recommended to only run $setCompData(compData) 
+##      as part of computing a solution for your working data. Having 
+##      a !NULL value returned may create problems with computed results.
 ##
 ## cacheSolve(x, ...)
 ##      Runs a matrix inversion on data generated using the 
@@ -34,8 +41,16 @@
 ##      then return the output. You can retrieve the output in 1 of 2
 ##      ways ('xSolve' will be the matrix containing the inversion):
 ##
-##      1) xSolve <- x$getSolve()
+##      1) xSolve <- x$getCompData()
 ##      2) xSolve <- cacheSolve(x)
+##
+##      This function is a dumb function. It will only check for !NULL
+##      results in the cached computed data. If $setCompData() is used
+##      on the compatible list data prior to computing, the input matrix
+##      will not be solved and may return an incorrect result. Assuming
+##      your list data is 'x', run x$setCompData(NULL) to reset your cache.
+##      You can also run the full makeCacheMatrix() function again with
+##      your working data as an input.
 
 makeCacheMatrix <- function(x = matrix()) {
         ## Resets computed data to NULL
@@ -51,27 +66,27 @@ makeCacheMatrix <- function(x = matrix()) {
         get <- function() x
         
         ## Function - Returns computed data back to 'm'
-        setSolve <- function(solve) m <<- solve
+        setCompData <- function(compData) m <<- compData
         
         ## Function - Returns data from 'm'
-        getSolve <- function() m
+        getCompData <- function() m
 
         ## Returns list of functions
         list(set = set, get = get,
-             setSolve = setSolve,
-             getSolve = getSolve)
+             setCompData = setCompData,
+             getCompData = getCompData)
 }
 
 
 cacheSolve <- function(x) {
         
         ## Populate 'm' with computed data contained in the list environment
-        m <- x$getSolve()
+        m <- x$getCompData()
         
         ## If computed data exists in the list (value of 'm' is not null),
         ## then the data will be returned.
         if(!is.null(m)) {
-                message("getting cached data")
+                message("Using cached data.")
                 return(m)
         }
         
@@ -82,7 +97,7 @@ cacheSolve <- function(x) {
         m <- solve(data)
         
         ## Returns computed data back to list cache
-        x$setSolve(m)
+        x$setCompData(m)
         
         ## Return the value 'm'
         m
